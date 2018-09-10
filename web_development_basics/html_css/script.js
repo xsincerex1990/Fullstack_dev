5b233e19384672758278490fb6e5728c0ae3b434
Github Cesar <xsincerex1990@gmail.com>
2018-09-10 19:09:02 -0400

    HTML basics

--- web_development_basics/html_css/script.js
+++ web_development_basics/html_css/script.js
@@ -1,21 +1,31 @@
-let list = document.createElement('ul');
-let info = document.createElement('p');
-let html = document.querySelector('html');
+let things = document.createElement("ul");
+let info = document.createElement("p");
+//queryselector() returns the first element within the document that matches the specified selector
+let html = document.querySelector("html");
 
-info.textContent = 'Click anywhere outside the list to add a new list item. Click an existing list item to change its text to something else.';
+// textContent property represents the text content of a node and its descendants
+info.textContent = 'Click anywhere outside the list to add a new list item. Click an existing list item to change its text to something else.'
 
 document.body.appendChild(info);
-document.body.appendChild(list);
+document.body.appendChild(things);
 
-html.onclick = function() {
-    let listItem = document.createElement('li');
-    let listContent = prompt('What contents do you want on the list?');
-    listItem.textContent = listContent;
-    list.appendChild(listItem);
+html.onclick = function() { 
+    var listItem = document.createElement('li');
+    var listContent = prompt("what contents do you want bishhh?");
+    listItem.textContent = listContent
+    things.appendChild(listItem);
 
+    
     listItem.onclick = function(e) {
+        //StopPropagation() stops the bubbling of an event to parent elements, preventing any parent event handlers from being executed
+        //In this case it stops html.onclick from executing again therefore stoping  prompt("what contents do you want bishhh?")
+        //from running
+
+      
         e.stopPropagation();
-        let listContent = prompt('Enter new content for list');
+        var listContent = prompt("Enter new listings");
         this.textContent = listContent;
+        //points to this textcontent because of how it was initiated
+
     }
-} 
+}
