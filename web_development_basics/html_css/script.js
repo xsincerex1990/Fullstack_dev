c849c3c0fd1187edec32a152877ff73df60171bc
Github Cesar <xsincerex1990@gmail.com>
2018-09-11 15:57:06 -0400

    front end practice

--- web_development_basics/html_css/script.js
+++ web_development_basics/html_css/script.js
@@ -1,10 +1,10 @@
 let things = document.createElement("ul");
 let info = document.createElement("p");
 //queryselector() returns the first element within the document that matches the specified selector
-let html = document.querySelector("html");
+let html = document.querySelector("body");
 
 // textContent property represents the text content of a node and its descendants
-info.textContent = 'Click anywhere outside the list to add a new list item. Click an existing list item to change its text to something else.'
+things.textContent = 'Click anywhere outside the list to add a new list item. Click an existing list item to change its text to something else.'
 
 document.body.appendChild(info);
 document.body.appendChild(things);
